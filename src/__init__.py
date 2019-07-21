09c46c7470a728e9cf37ccedfb6f7855ff79b77a
JEF-PC\Jeferson Menegazzo <jeferson@ibs.inf.br>
2019-07-21 15:47:47 -0300

    Removido serial. Refatorado projeto.

--- src/__init__.py
+++ src/__init__.py
@@ -8,11 +8,11 @@ PRODUCTION = False
 # DEFAULT
 SAMPLING_RATE = 0.01 if PRODUCTION else 0.5 # 100 Hz or 2 Hz
 FOLDER = "../../data"
-FILE = FOLDER + "/data-set " + datetime.datetime.fromtimestamp(time.time()).strftime('%d-%m-%Y %H-%M-%S') + ".csv"
+FILE = FOLDER + "/mpu-data-set " + datetime.datetime.fromtimestamp(time.time()).strftime('%d-%m-%Y %H-%M-%S') + ".csv"
 
 def showSettings():
     print("Sensor Data Collect")
-    print("Mode: " + ("production" if PRODUCTION else "dev"))
+    print("Mode: " + ("production" if PRODUCTION else "development"))
     print("Sampling Rate: " + str(SAMPLING_RATE))
     print("Console Enabled: " + str(CONSOLE))
     print("Data Folder: " + FOLDER)
