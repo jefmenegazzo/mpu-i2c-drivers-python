96838618e55efa642bdeccec928533702fe3049e
Jeferson Menagazzo <jef.menegazzo@outlook.com>
2020-07-17 18:46:56 -0300

    Update version.

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mpu9250_jmdev",
-    version="1.0.10",
+    version="1.0.11",
     author="Jeferson Menegazzo",
     author_email="jef.menegazzo@outlook.com",
     description="MPU-9250 (MPU-6500 + AK8963) I2C Driver in Python for Raspbery PI",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/jefmenegazzo/MPU9250",
+    url="https://github.com/Intelligent-Vehicle-Perception/MPU-9250-Sensors-Data-Collect",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
